b244c101064561868b1d0ea7bdcc26908b2f57bc
skaggej <eskaggs@outlook.com>
2015-11-24 10:24:51 -0700

    Added

---
+++ SharePointNerdBlog/SharePointNerdBlog/ER_BlogCommentsNew/ER_BlogCommentsNew.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.SharePoint;
+
+namespace SharePointNerdBlog.ER_BlogCommentsNew
+{
+    /// <summary>
+    /// List Item Events
+    /// </summary>
+    public class ER_BlogCommentsNew : SPItemEventReceiver
+    {
+       /// <summary>
+       /// An item is being added.
+       /// </summary>
+        public override void ItemAdding(SPItemEventProperties properties)
+        { 
+            SPWeb rootWeb = properties.Web.Site.RootWeb;
+            try
+            {
+                string validMessage = properties.AfterProperties["Validation"].ToString();
+                if (validMessage == "Colorless Green Ideas Sleep Furiously")
+                {
+                    base.ItemAdding(properties);
+                }
+                else
+                {
+                    //Cancels adding the item
+                    properties.Cancel = true;
+                    properties.ErrorMessage = "Your comment was not submitted successfully.  Please ensure you've selected the correct validation message and try again.  Thank you.";
+                    return;
+                }
+            }
+            catch (Exception exception)
+            {
+                SPList errorsList = rootWeb.Lists["Errors"];
+                SPListItem newError = errorsList.Items.Add();
+                newError["Message"] = exception.Message;
+                newError["Source"] = exception.Source;
+                newError["StackTrace"] = exception.StackTrace;
+                newError["InnerException"] = exception.InnerException;
+                newError.Update();
+                return;  //ensures the error only occurs once
+            }
+        }
+    }
+}
---
+++ SharePointNerdBlog/SharePointNerdBlog/Features/SharePointNerd_Blog/SharePointNerd_Blog.feature
@@ -0,0 +1,6 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="8ed13a98-056b-4e4d-bcbb-983d0fa3a28c" activateOnDefault="false" description="This feature contains event receivers that are specific to the blog site within SharePointNerd.com." featureId="8ed13a98-056b-4e4d-bcbb-983d0fa3a28c" imageUrl="../../WebGraphics/Iconic Small.jpg" solutionId="00000000-0000-0000-0000-000000000000" title="SharePointNerd_Blog" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="f8a607c4-108f-4125-9d67-53c5047e66cc" />
+  </projectItems>
+</feature>
